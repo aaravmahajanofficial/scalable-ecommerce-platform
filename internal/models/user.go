e4fbfaa028f874dc4ad54976372e028fd44f55c1
Aarav Mahajan <aaravmahajan2003@gmail.com>
2025-02-17 20:18:38 +0530

    update the user models

---
+++ internal/models/user.go
@@ -0,0 +1,59 @@
+package types
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type User struct {
+	ID        string    `json:"id"`
+	Name      string    `json:"name" validate:"required"`
+	Username  string    `json:"username" validate:"required"`
+	Email     string    `json:"email" validate:"required"`
+	Password  string    `json:"-"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"update_at"`
+}
+
+// for registration
+type Registeration struct {
+	Email    string `json:"email" validate:"required, email"`
+	Password string `json:"password" validate:"required, min=6"`
+	Name     string `json:"name" validate:"required"`
+}
+
+// for login
+type Login struct {
+	Email    string `json:"email" validate:"required, email"`
+	Password string `json:"password" validate:"required"`
+}
+
+// JWT claims structure
+
+type Claims struct {
+	UserID int64  `json:"user_id"`
+	Email  string `json:"email"`
+	jwt.RegisteredClaims
+}
+
+/*
+
+Registered claims
+The JWT specification defines seven reserved claims that are not required, but are recommended to allow interoperability with third-party applications. These are:
+
+iss (issuer): Issuer of the JWT
+
+sub (subject): Subject of the JWT (the user)
+
+aud (audience): Recipient for which the JWT is intended
+
+exp (expiration time): Time after which the JWT expires
+
+nbf (not before time): Time before which the JWT must not be accepted for processing
+
+iat (issued at time): Time at which the JWT was issued; can be used to determine age of the JWT
+
+jti (JWT ID): Unique identifier; can be used to prevent the JWT from being replayed (allows a token to be used only once)
+
+*/
