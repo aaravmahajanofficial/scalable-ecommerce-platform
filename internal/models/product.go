0d5465a38d71467842860467c92a92077fc688a6
Aarav Mahajan <aaravmahajan2003@gmail.com>
2025-02-20 00:52:11 +0530

    add product catalog struct

---
+++ internal/models/product.go
@@ -0,0 +1,42 @@
+package models
+
+import "time"
+
+type Category struct {
+	ID          int64     `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
+
+type Product struct {
+	ID            int64     `json:"id"`
+	Name          string    `json:"name"`
+	Description   string    `json:"description"`
+	CreatedAt     time.Time `json:"created_at"`
+	UpdatedAt     time.Time `json:"updated_at"`
+	Category      *Category `json:"category,omitempty"`
+	Price         float64   `json:"price"`
+	StockQuantity int64     `json:"stock_quantity"`
+	SKU           string    `json:"sku"`
+	Status        string    `json:"status"`
+}
+
+type CreateProductRequest struct {
+	CategoryID    int64   `json:"category_id" validate:"required"`
+	Name          string  `json:"name,omitempty" validate:"omitempty,min=3,max=200"`
+	Description   string  `json:"description,omitempty"`
+	Price         float64 `json:"price,omitempty" validate:"omitempty, gt=0"`
+	StockQuantity int     `json:"stock_quantity" validate:"required,gte=0"`
+	SKU           string  `json:"sku" validate:"required,min=3,max=50"`
+}
+
+type UpdateProductRequest struct {
+    CategoryID    *int64   `json:"category_id,omitempty"`
+    Name          *string  `json:"name,omitempty" validate:"omitempty,min=3,max=200"`
+    Description   *string  `json:"description,omitempty"`
+    Price         *float64 `json:"price,omitempty" validate:"omitempty,gt=0"`
+    StockQuantity *int     `json:"stock_quantity,omitempty" validate:"omitempty,gte=0"`
+    Status        *string  `json:"status,omitempty" validate:"omitempty,oneof=active inactive discontinued"`
+}
