3d89d60893d7d939c6cc671a490ff2b3b7f8256d
Aarav Mahajan <aaravmahajan2003@gmail.com>
2025-02-21 00:36:11 +0530

    implement productService

---
+++ internal/service/product.go
@@ -0,0 +1,98 @@
+package service
+
+import (
+	"github.com/aaravmahajanofficial/scalable-ecommerce-platform/internal/models"
+	"github.com/aaravmahajanofficial/scalable-ecommerce-platform/internal/repository"
+)
+
+type ProductService struct {
+	repo *repository.ProductRepository
+}
+
+func NewProductService(repo *repository.ProductRepository) *ProductService {
+	return &ProductService{repo: repo}
+}
+
+func (s *ProductService) CreateProduct(req *models.CreateProductRequest) (*models.Product, error) {
+
+	product := &models.Product{
+		CategoryID:    req.CategoryID,
+		Name:          req.Name,
+		Description:   req.Description,
+		Price:         req.Price,
+		StockQuantity: req.StockQuantity,
+		SKU:           req.SKU,
+		Status:        "active",
+	}
+
+	err := s.repo.CreateProduct(product)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+
+}
+
+func (s *ProductService) GetProductByID(id int64) (*models.Product, error) {
+
+	return s.repo.GetProductByID(id)
+
+}
+
+func (s *ProductService) UpdateProductRequest(id int64, req *models.UpdateProductRequest) (*models.Product, error) {
+
+	product, err := s.repo.GetProductByID(id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if req.CategoryID != nil {
+		product.CategoryID = *req.CategoryID
+	}
+	if req.Name != nil {
+		product.Name = *req.Name
+	}
+	if req.Description != nil {
+		product.Description = *req.Description
+	}
+	if req.Price != nil {
+		product.Price = *req.Price
+	}
+	if req.StockQuantity != nil {
+		product.StockQuantity = *req.StockQuantity
+	}
+	if req.Status != nil {
+		product.Status = *req.Status
+	}
+
+	err = s.repo.UpdateProduct(product)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return product, err
+
+}
+
+// page means "page number requested"
+// pageSize means "number of products to be displayed per page"
+func (s *ProductService) ListProducts(page, pageSize int) ([]*models.Product, error) {
+
+	if page < 1 {
+		page = 1
+	}
+
+	if pageSize < 1 || pageSize > 100 {
+		pageSize = 10
+
+	}
+
+	offset := (page - 1) * pageSize
+
+	return s.repo.ListProducts(offset, pageSize)
+
+}
